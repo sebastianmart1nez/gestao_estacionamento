df8866e3fd40a629b42c87ad06ab7aa7dc946a60
sebastianmart1nez <161771398+sebastianmart1nez@users.noreply.github.com>
2026-04-02 16:36:27 +0100

    Senha do utilizador admin modificada

--- src/SistemaLogin.java
+++ src/SistemaLogin.java
@@ -1,3 +1,8 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 public class SistemaLogin {
 
     public boolean autenticar(String utilizador, String senha){
@@ -5,5 +10,27 @@ public class SistemaLogin {
             return false;
         }
         return utilizador.equals("admin") && senha.equals("1234");
+    }
+
+    public static class SistemaLoginTeste {
+
+        //Baby step
+
+        @Test
+        public void deveAutenticarUtilizadorValido(){
+            SistemaLogin login = new SistemaLogin();
+
+            boolean resultado = login.autenticar("admin","Admin@2026");
+            assertTrue(resultado);
+        }
+        @Test
+        public void deveRejeitarUtilizadorInvalido(){
+            SistemaLogin login = new SistemaLogin();
+
+            boolean resultado = login.autenticar("user", "0000");
+            assertFalse(resultado);
+        }
+
+
     }
 }
